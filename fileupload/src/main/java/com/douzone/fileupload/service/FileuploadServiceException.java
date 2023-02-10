568d552a1d4c362bd550e176c966db5189afa55d
kimki0917 <kimki0917@naver.com>
2023-02-10 20:18:50 +0900

    junit

---
+++ fileupload/src/main/java/com/douzone/fileupload/service/FileuploadServiceException.java
@@ -0,0 +1,16 @@
+package com.douzone.fileupload.service;
+
+public class FileuploadServiceException extends RuntimeException {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public FileuploadServiceException(String message) {
+		super(message);
+	}
+
+	public FileuploadServiceException() {
+		super("Fileupload Exception Occurs");
+	}
+}
